79637d9fee3d2219990ac23004bd5f6c2470cdc8
Michael-Kamau <kamau.karitu@gmail.com>
2020-01-07 20:58:24 +0300

    modified the index file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use myNamespace\Student ;
 $persons = array("Peter"=>1935, "Ben"=>2000, "Joe"=>1990);
 
 foreach($persons as $person => $person_dob) {
-    echo "<h1>Student</h1>";
+    echo "<h1>Student Object</h1>";
     $student=new Student($person, $person_dob);
     if($student->getAge()> 50){
         $student->kill();
